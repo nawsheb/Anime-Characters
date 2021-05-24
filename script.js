7c0bec884b085438f8d66acb501ec650b7bcb79b
nawsheb <nasheeb2014@gmail.com>
2021-05-24 15:10:44 -0700

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(".submt").click(function() {
     var pics = $(".pic-url").val();
     imgs.push(pics);
       
-      if (pics = pics.match(urlRegex)) {
+      if (pics === pics.match(urlRegex)) {
         
         $(".images").append('<div class="box"><div class="imgBox"><img src="' + pics + '" ></div> </div>');
         
