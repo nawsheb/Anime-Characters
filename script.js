54ba7786084ddd2003f323841b82ef9e9ca6f670
nawsheb <nasheeb2014@gmail.com>
2021-05-24 15:50:10 -0700

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let imgs = [];
-var urlRegex = /((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z0-9\&\.\/\?\:@\-_=#])*/;
+
+
 $(".submt").click(function() {
     var pics = $(".pic-url").val();
     imgs.push(pics);
-      
+      var urlRegex = /((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z0-9\&\.\/\?\:@\-_=#])*/;
       if (pics = pics.match(urlRegex)) {
         
         $(".images").append('<div class="box"><div class="imgBox"><img src="' + pics + '" ></div> </div>');
