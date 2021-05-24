c311aac004cd30a10b5640cd92021b4898c2bb3e
nawsheb <nasheeb2014@gmail.com>
2021-05-24 14:01:46 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,18 @@
+let imgs = [];
+var urlRegex = /((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z0-9\&\.\/\?\:@\-_=#])*/;
+$(".submt").click(function() {
+    var pics = $(".pic-url").val();
+    imgs.push(pics);
+      
+      if (pics = pics.match(urlRegex)) {
+        
+        $(".images").append('<div class="box"><div class="imgBox"><img src="' + pics + '" ></div> </div>');
+        
+      }
+  
+  else {
+     $(".invalid").text("Invalid Input");
+  }
+   
+    
+});
